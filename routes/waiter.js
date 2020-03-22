42fddcf03e3c108673ea2753ecd4704ca6fb121f
17003950 <webdevprojectmmu@gmail.com>
2020-03-22 21:47:36 +0000

    Order now writes to database.

--- routes/waiter.js
+++ routes/waiter.js
@@ -4,6 +4,10 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize('mysql://'+process.env.DBUSERNAME+':'+process.env.DBPASSWORD+'@'+process.env.DBURL+':'+process.env.DBPORT+'/'+process.env.DATABASE+'');
 const Item = sequelize.import("../model/item");
 const OrderItems = sequelize.import("../model/order_item");
+const Order = sequelize.import("../model/order");
+
+Order.belongsToMany(Item,{foreignKey:"order_id", foreignKeyConstraint: true,through:OrderItems})
+Item.belongsToMany(Order,{foreignKey: "item_id", foreignKeyConstraint: true,through:OrderItems})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,9 +16,20 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   console.log("Post made");
   let order = req.body;
   console.log(order);
+
+  const theOrder = await Order.create({ staff_id: 2, table_num: 12 });
+  console.log(theOrder instanceof Order); // true
+  console.log(theOrder.toJSON());
+
+  for(let i = 0; i < order.length; i++) {
+    const theItems = await OrderItems.create({ order_id: theOrder.order_id, item_id: order[i].item_id, quantity: order[i].quantity });
+    console.log(theItems instanceof OrderItems); // true
+    console.log(theItems.toJSON());
+  }
+
 });
 module.exports = router;
